1980744fc96c915493b6a465346eecb0b19fe4d0
zhangsheng261 <zhangsheng261@163.com>
2025-05-27 16:10:09 +0800

    Update WebConfig.js

--- static/config/WebConfig.js
+++ static/config/WebConfig.js
@@ -1,8 +1,8 @@
 let WebConfig = window.WebConfig = {
   //主地址 //用于集成zuul网关部署时注册、登录、登出使用 //如未集成zuul需与servicesUrl一致
-  'ServicesIpPort':'http://rgis.igcloud.tech:16000/',//http://10.8.7.115:16000  http://localhost:15002
+  'ServicesIpPort':'http://121.36.245.56:16000/',//http://10.8.7.115:16000  http://localhost:15002
   //服务地址
-  'ServicesUrl':"http://rgis.igcloud.tech:16000/GeoPhyServer",  //WebConfig.ServicesIpPort+"/GeoPhyServer" //172.30.17.101:8080/visualservice http://172.30.17.101:8080/visualservice  http://localhost:15002 http://localhost:15004 http://10.8.7.115:16000/GeoPhyServer
+  'ServicesUrl':"http://121.36.245.56:16000/GeoPhyServer",  //WebConfig.ServicesIpPort+"/GeoPhyServer" //172.30.17.101:8080/visualservice http://172.30.17.101:8080/visualservice  http://localhost:15002 http://localhost:15004 http://10.8.7.115:16000/GeoPhyServer
   //用户手册url
   'webUrl':'http://172.30.17.101:8080/GeoPhysical/static/userHelp/userHelp.pdf',
   //本地算法程序下載地址
